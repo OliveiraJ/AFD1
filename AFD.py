2bcdf2b71a183aed52958a8caf5143b729830150
OliveiraJ <jordansilva102@gmail.com>
2021-12-05 19:02:55 -0300

    second commit

---
+++ AFD.py
@@ -0,0 +1,33 @@
+estds = input().split(" ")
+simb_alfabeto = input().split(" ")
+qtd_transicoes = int(input())
+
+transicoes_afd = {}
+
+for estd in estds:
+    transicoes_afd[estd] = {}
+
+for i in range(0,qtd_transicoes):
+    inicial, character, final = input().split(" ")
+    
+    if character not in transicoes_afd[inicial]:
+        transicoes_afd[inicial][character] = final
+        
+estd_inicial = input()
+estds_finais = input().split(" ")
+sigma = input().split(" ")
+
+for palavra in sigma:
+    estd_atual = estd_inicial
+    estd_erro = 0
+    
+    for letra in palavra:
+        try:
+            estd_atual = transicoes_afd[estd_atual][letra]
+        except KeyError:
+            estd_erro = 1
+            break
+    if(estd_erro == 1 or estd_atual not in estds_finais):
+        print('N')
+    else:
+        print('S')
